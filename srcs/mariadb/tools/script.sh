01361a4fdf9642d7f5db6a1ecb16343606a6af81
NuIled <sifi6027@gmail.com>
2023-12-24 22:05:10 +0100

    fixed mariadb probleme and missing packages on wp

--- srcs/mariadb/tools/script.sh
+++ srcs/mariadb/tools/script.sh
@@ -5,17 +5,22 @@ mkdir -p /run/mysqld
 
 chown -R mysql:mysql /run/mysqld
 
-sleep 200000
 chown -R mysql:mysql /var/lib/mysql
 mkdir -p /run/openrc/
 touch /run/openrc/softlevel
 
 # chown -R mysql:mysql /auth_pam_tool_dir
-rm -rf /var/lib/mysql/*
-mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql > /dev/null
+#mysql_secure_installation --basedir=/usr --datadir=/var/lib/mysql --user=mysql 
 rc-service mariadb setup
+echo "USE mysql;
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'PWD';
+CREATE DATABASE WP CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'USR'@'%' IDENTIFIED by 'PWD';
+GRANT ALL PRIVILEGES ON WP.* TO 'USR'@'%';
+FLUSH PRIVILEGES;" > /tmp/file
 
-
+mysqld --user=mysql --bootstrap < /tmp/file
 
 # #!/bin/sh
 # #!/bin/sh
@@ -30,11 +35,8 @@ rc-service mariadb setup
 # 	# run init.sql
 
 # # allow remote connections
-# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-mysqld --initialize --user=mysql
-mysql -u root -e "FLUSH PRIVILEGES;ALTER USER 'root'@'localhost';CREATE DATABASE WP ;CREATE USER 'USR'@'%' IDENTIFIED by 'PWD';GRANT ALL PRIVILEGES ON WP.* TO 'USR'@'%';"
-#pkill mysqld
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+ sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 mysqld --user=mysql --console 
 #mysqld --user=mysql --console
 
--- srcs/wordpress/tools/script.sh
+++ srcs/wordpress/tools/script.sh
@@ -20,20 +20,18 @@ chmod +x /bin/wp
 mv /usr/sbin/php-fpm81 /bin/php-fpm
 
 
-    wp core download --allow-root
+wp core download --allow-root
 # wait for mysql
-while ! wp config create --dbname="WP" --dbuser="USR" --dbpass="PWD" --dbhost="mariadb" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root > /dev/null
+while ! wp config create --dbname="$MYSQL_DB" --dbuser="$MYSQL_USR" --dbpass="$MYSQL_PWD" --dbhost="$MYSQL_HOST" --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root > /dev/null
 do    
-    echo notfoubd;
+    echo not foubd
     sleep 3
 done
-    wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-    wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-    wp theme install inspiro --activate --allow-root
-    wp plugin install redis-cache --activate --allow-root
-    wp plugin update --all --allow-root
-
-
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+#wp theme install inspiro --activate --allow-root
+#wp plugin install redis-cache --activate --allow-root
+#wp plugin update --all --allow-root
 
 echo "Wordpress started on :9000"
 
